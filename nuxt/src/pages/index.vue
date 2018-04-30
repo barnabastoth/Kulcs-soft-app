48748e94f64ea6d9ae14efc977368d0efa007133
Unknown <barnabastoth94@gmail.com>
2018-04-30 23:56:25 +0200

    Main technologies added to about card in Index

--- nuxt/src/pages/index.vue
+++ nuxt/src/pages/index.vue
@@ -12,7 +12,41 @@
       </q-card-title>
       <q-list separator>
         <q-collapsible icon="explore" label="Az applikáció során felhasznált főbb technológiák">
-          Lorem ipsum dolor sit amet...
+          <div>
+            <q-list>
+              <q-list-header inset>Frontend</q-list-header>
+              <q-item>
+                <q-item-side avatar="statics/vue.png" />
+                <q-item-main label="Vue JS" />
+              </q-item>
+              <q-item>
+                <q-item-side avatar="statics/nuxt.png" />
+                <q-item-main label="Nuxt" />
+              </q-item>
+              <q-item>
+                <q-item-side avatar="statics/quasar.jpg" />
+                <q-item-main label="Quasar Framework" />
+              </q-item>
+              <q-item-separator inset />
+              <q-list-header inset>Backend</q-list-header>
+              <q-item>
+                <q-item-side avatar="statics/spring-boot.png" />
+                <q-item-main label="Spring Boot" />
+              </q-item>
+              <q-item>
+                <q-item-side avatar="statics/spring-security.png" />
+                <q-item-main label="Spring Security" />
+              </q-item>
+              <q-item>
+                <q-item-side avatar="statics/postgresql.png" />
+                <q-item-main label="PostgreSQL" />
+              </q-item>
+              <q-item>
+                <q-item-side avatar="statics/hibernate.jpg" />
+                <q-item-main label="Hibernate" />
+              </q-item>
+            </q-list>
+          </div>
         </q-collapsible>
 
         <q-collapsible icon="perm_identity" label="Second">
