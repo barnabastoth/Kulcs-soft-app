f37a3c49e452dbfb321988cc26a38f993a119294
Unknown <barnabastoth94@gmail.com>
2018-04-30 10:21:51 +0200

    Basic login form implemented

---
+++ nuxt/src/pages/authentication.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page>
+        <div style="width: 700px; max-width: 90vw;" class="absolute-center">
+          <div
+            class="main-color shadow-1 row inline flex-center text-white"
+            :class="`bg-black`"
+          >
+          </div>
+          <q-input float-label="Felhasználónév" inverted color="primary" type="email" :before="[{icon: 'fa-address-card', handler () {}}]"/>
+          <br>
+          <q-input float-label="Jelszó" inverted color="primary" type="password" :before="[{icon: 'fa-key'}]" :after="[{icon: 'done', condition: login.password.length >= 5, handler () {}}]" />
+          <br>
+          <q-btn color="primary" icon-right="fa-sign-in-alt" label="Bejelentkezés" />
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+    export default {
+        name: "authentication"
+    }
+</script>
+
+<style scoped>
+
+</style>
