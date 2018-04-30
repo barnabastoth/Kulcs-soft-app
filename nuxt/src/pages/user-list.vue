f6e802ca510eef0221668744e56e9258ff34feff
Unknown <barnabastoth94@gmail.com>
2018-04-30 20:24:09 +0200

    UserList page design implemented

---
+++ nuxt/src/pages/user-list.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <q-table
+      :data="tableData"
+      :columns="columns"
+      selection="multiple"
+      :selected.sync="selectedUsers"
+      row-key="name"
+      color="secondary"
+      title="Válaszd ki a törölni kivánt felhasználót(kat)"
+    >
+      <template slot="top-selection" slot-scope="props">
+        <q-btn icon="delete" color="negative" label="Kijelölt felhasználók törlése" class="q-mr-sm" />
+      </template>
+    </q-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user-list'
+}
+</script>
