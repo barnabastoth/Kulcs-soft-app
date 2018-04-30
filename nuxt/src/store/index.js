dfa660f20ba6435ef28b34eaf532445661f3e8b9
Unknown <barnabastoth94@gmail.com>
2018-04-30 09:50:44 +0200

    Login mutation implemented in Store

--- nuxt/src/store/index.js
+++ nuxt/src/store/index.js
@@ -3,10 +3,19 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+const LOGOUT = 'LOGOUT'
+
 const store = new Vuex.Store({
   state: {
     isLoggedIn: !!localStorage.getItem('token'),
     loggedInUser: []
+  },
+  mutations: {
+    [LOGIN_SUCCESS] (state, user) {
+      state.isLoggedIn = true
+      state.loggedInUser = user
+    }
   }
 })
 
